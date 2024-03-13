cfd4e30cd655bc76422b31737106ef092fe61a68
Luka Lickah <lgc032@einstein.br>
2024-03-13 19:31:37 -0400

    socialite config

---
+++ resources/views/sessions/login.blade.php
@@ -0,0 +1,112 @@
+<x-layout bodyClass="bg-gray-200">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                <!-- Navbar -->
+                <!-- <x-navbars.navs.guest signin='login' signup='register'></x-navbars.navs.guest>
+                <!-- End Navbar -->
+            </div>
+        </div>
+    </div>
+    <main class="main-content  mt-0">
+        <div class="page-header align-items-start min-vh-100"
+            style="background-image: url('https://jcconcursos.com.br/media/_versions/noticia/ifms_widelg.jpg');">
+            <span class="mask bg-gradient-dark opacity-6"></span>
+            <div class="container mt-5">
+                <div class="row signin-margin">
+                    <div class="col-lg-4 col-md-8 col-12 mx-auto">
+                        <div class="card z-index-0 fadeIn3 fadeInBottom card-color-custom">
+                            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 ">
+                                    <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">SOHPE - Sistema
+                                        Gerenciador de Horários de Permanência</h4>
+                                    <div class="row mt-3 justify-content-center">
+                                        {{-- <h6 class='text-white text-center mb-3'>
+                                            <span class="font-weight-normal">Email:</span> admin@material.com
+                                            <br>
+                                            <span class="font-weight-normal">Password:</span> secret
+                                        </h6> --}}
+                                        <div class="row justify-content-center mb-4">
+                                            <button class="google-btn">
+                                                <div class="google-icon">
+                                                    <i class="fab fa-google"></i>
+                                                </div>
+                                                Logar com o Google
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <form role="form" method="POST" action="{{ route('login') }}"
+                                        class="text-start">
+                                        @csrf
+                                        @if (Session::has('status'))
+                                            <div class="alert alert-success alert-dismissible text-white"
+                                                role="alert">
+                                                <span class="text-sm">{{ Session::get('status') }}</span>
+                                                <button type="button" class="btn-close text-lg py-3 opacity-10"
+                                                    data-bs-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                        @endif
+                                        <div class="input-group input-group-outline mt-3">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" name="email"
+                                                value="{{ 'luka.ferreira@estudante.ifms.edu.br' }}">
+                                        </div>
+                                        @error('email')
+                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                        @enderror
+                                        <div class="input-group input-group-outline mt-3">
+                                            <label class="form-label">Senha</label>
+                                            <input type="password" class="form-control" name="password"
+                                                value='{{ 'secret' }}'>
+                                        </div>
+                                        @error('password')
+                                            <p class='text-danger inputerror'>{{ $message }} </p>
+                                        @enderror
+                                        {{-- <div class="form-check form-switch d-flex align-items-center my-3">
+                                            <input class="form-check-input" type="checkbox" id="rememberMe">
+                                            <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember
+                                                me</label>
+                                        </div> --}}
+                                        <div class="text-center">
+                                            <button type="submit"
+                                                class="btn bg-gradient-primary w-100 my-4 mb-2 btn-no-hover">Logar</button>
+                                        </div>
+                                        <p class="mt-4 text-sm text-center">
+                                            Não tem uma conta?
+                                            <a href="{{ route('register') }}"
+                                                class="text-primary text-gradient font-weight-bold">Registrar</a>
+                                        </p>
+                                        <p class="text-sm text-center">
+                                            Esqueceu sua senha? Redefinir sua senha
+                                            <a href="{{ route('verify') }}"
+                                                class="text-primary text-gradient font-weight-bold">aqui</a>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </main>
+    @push('js')
+        <script src="{{ asset('assets') }}/js/jquery.min.js"></script>
+        <script>
+            $(function() {
+
+                var text_val = $(".input-group input").val();
+                if (text_val === "") {
+                    $(".input-group").removeClass('is-filled');
+                } else {
+                    $(".input-group").addClass('is-filled');
+                }
+            });
+        </script>
+    @endpush
+</x-layout>
